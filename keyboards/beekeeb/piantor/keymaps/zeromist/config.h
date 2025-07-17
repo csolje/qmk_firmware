bc92ceacab13a9d5af810c528a40c53fa3f9ad24
Christian Solje <chsl@bang-olufsen.dk>
2025-07-17 08:12:29 +0200

    fix: 🐛 Changed keymaps and config

--- keyboards/beekeeb/piantor/keymaps/zeromist/config.h
+++ keyboards/beekeeb/piantor/keymaps/zeromist/config.h
@@ -7,15 +7,18 @@
 #define BOOTMAGIC_COLUMN 5
 
 // Configure the global tapping term (default: 200ms)
-//#define TAPPING_TERM 185
+#define TAPPING_TERM 185
 #define TAPPING_TERM_PER_KEY
+#define TAPPING_TOGGLE 5
+#define QUICK_TAP_TERM 120
+#define QUICK_TAP_TERM_PER_KEY
+
 // Prevent normal rollover on alphas from accidentally triggering mods.
 
 #define ONESHOT_TAP_TOGGLE 5  /* Tapping this number of times holds the key until tapped once again. */
 #define ONESHOT_TIMEOUT 5000  /* Time (in ms) before the one shot key is released */
 
 // Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
-#define TAPPING_FORCE_HOLD
 
 #define COMBO_COUNT 2
 #define SPLIT_USB_DETECT
